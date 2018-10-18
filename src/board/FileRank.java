a239bf01054fa3e7c7ac11b1f6a4298e69eecae9
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-18 16:58:28 -0400

    Added toString() to FileRank.java

--- src/board/FileRank.java
+++ src/board/FileRank.java
@@ -1,11 +1,17 @@
 package board;
 
 public class FileRank {
-	int file;
-	int rank;
+	char file; // a, b, c, d, e, f, g, h
+	int rank; // 1, 2, 3, 4, 5, 6, 7, 8
 	
-	public FileRank(int file, int rank) {
+	public FileRank(char file, int rank) {
 		this.file = file;
 		this.rank = rank;
+	}
+	
+	public String toString() {
+		String result = "File: " + file;
+		result += "\nRank: " + rank;
+		return result;
 	}
 }
