ed7a8dfc25d22e3211d60d8b25afada62ad01545
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-18 01:38:50 -0400

    Fixed printing of Board and populated chess board

--- src/board/Board.java
+++ src/board/Board.java
@@ -9,7 +9,31 @@ public class Board {
 		populateBoard();
 	}
 	private void populateBoard() {
+		board[0][0] = new Rook('w', 'R');
+		board[0][1] = new Knight('w', 'N');
+		board[0][2] = new Bishop('w', 'B');
+		board[0][3] = new Queen('w', 'Q');
+		board[0][4] = new King('w', 'K');
+		board[0][5] = new Bishop('w', 'B');
+		board[0][6] = new Knight('w', 'N');
+		board[0][7] = new Rook('w', 'R');
 		
+		for(int i = 0; i < 8; i++) {
+			board[1][i] = new Pawn('w', 'p');
+		}
+		
+		board[7][0] = new Rook('b', 'R');
+		board[7][1] = new Knight('b', 'N');
+		board[7][2] = new Bishop('b', 'B');
+		board[7][3] = new Queen('b', 'Q');
+		board[7][4] = new King('b', 'K');
+		board[7][5] = new Bishop('b', 'B');
+		board[7][6] = new Knight('b', 'N');
+		board[7][7] = new Rook('b', 'R');
+		
+		for(int i = 0; i < 8; i++) {
+			board[6][i] = new Pawn('b', 'p');
+		}
 	}
 	public void printBoard() {
 		for(int i = 0; i < board.length; i++) {
@@ -23,11 +47,12 @@ public class Board {
 				} else {
 					System.out.print(board[i][j]);
 				}
+				System.out.print(" ");
 			}
 			System.out.println(" " + (8 - i));
 		}
 		for(int i = 0; i < 8; i++) {
-			System.out.print(" " + file[i]);
+			System.out.print(" " + file[i] + " ");
 		}
 	}
 
