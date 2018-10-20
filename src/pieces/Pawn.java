6c9099a829073599576498aa4b5cfeb74487151d
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-20 01:55:51 -0400

    Started isLegalMove method in Pawn

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -1,17 +1,35 @@
 package pieces;
 
 import board.FileRank;
-
+/**
+ * 
+ * @author Sarah Squillace, Nikita Zala
+ * Pawns cannot move backward. 
+ * 
+ * En Passant: Can only be done the turn after the pawn uses its 2 square
+ * initial move. 
+ */
 public class Pawn extends Piece {
-
+	boolean hasMoved;
+	boolean justMoved2Squares;
 	public Pawn(char team, char type) {
 		super(team, type);
+		hasMoved = false;
+		justMoved2Squares = false;
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean isValidMove(FileRank start, FileRank end) {
-		// TODO Auto-generated method stub
+		if(Piece.getBoard().get(end).getTeam() == team) {
+			return false;
+		}
+		if(team == 'w') { // if white (can only move down)
+			
+		} else { // if black (can only move up)
+			
+		}
+		
 		return false;
 	}
 
