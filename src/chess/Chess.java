5eaa6af86379bb00649900e7261fc4d083cea863
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-25 01:14:54 -0400

    Added comment to model chess board

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -9,7 +9,16 @@ public class Chess {
 		Board board = new Board();
 		board.printBoard();
 		System.out.println();
-
+		
 		// should probably put game loop in this class
 	}
 }
+// bR bN bB bQ bK bB bN bR  8
+// bp bp bp bp bp bp bp bp  7
+//    ##    ##    ##    ##  6
+// ##    ##    ##    ##     5
+//    ##    ##    ##    ##  4
+// ##    ##    ##    ##     3
+// wp wp wp wp wp wp wp wp  2
+// wR wN wB wQ wK wB wN wR  1
+//  a  b  c  d  e  f  g  h 
