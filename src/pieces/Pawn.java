9e6bb3b73e45b6d72f8a9fb821928c6bd03c366c
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-25 01:16:01 -0400

    Added to isLegalMove and added helper methods

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -10,8 +10,8 @@ import board.FileRank;
  * initial move. 
  */
 public class Pawn extends Piece {
-	boolean hasMoved;
-	boolean justMoved2Squares;
+	boolean hasMoved; // false if the pawn has never moved
+	boolean justMoved2Squares; // only true the turn after a pawn uses its 2 square move
 	public Pawn(char team, char type) {
 		super(team, type);
 		hasMoved = false;
@@ -21,19 +21,56 @@ public class Pawn extends Piece {
 
 	@Override
 	public boolean isValidMove(FileRank start, FileRank end) {
-		if(Piece.getBoard().get(end).getTeam() == team) { // can't go to same square as team
+
+		if (is2SquareMove(start, end)) { // COMPLETE
+			return true;
+		}
+		else if (isCapture(start, end)) { // also considers if capture is en passant
+			return true;
+		}
+		else if (isRegularMove(start, end)) { // COMPLETE
+			return true;
+		}
+		else {
 			return false;
 		}
-
-		return false;
 	}
 	private boolean isEnPassant (FileRank start, FileRank end) {
 		return false;
 	}
 	private boolean is2SquareMove (FileRank start, FileRank end) {
+		if(!hasMoved) {
+			if(team == 'w') { // if white
+				if (end.getRank() - start.getRank() == 2 && start.getFile() == end.getFile()) { // pawn moved 
+					return true;
+				}
+			} else { // if black
+				if (end.getRank() - start.getRank() == -2 && start.getFile() == end.getFile()) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	private boolean isCapture (FileRank start, FileRank end) {
+		if(isEnPassant(start, end)) {
+			
+		}
 		return false;
+	}
+	private boolean isRegularMove (FileRank start, FileRank end) {
+		if (Piece.getBoard().get(end) == null) { // pawns can only move onto empty spaces
+			if(team == 'w') { // if pawn is white
+				if(end.getRank() - start.getRank() == 1 && start.getFile() == end.getFile()) { // making sure the square is correct
+					return true;
+				}
+			} else { // if pawn is black
+				if(end.getRank() - start.getRank() == -1 && start.getFile() == end.getFile()) { // making sure the square is correct
+					return true;
+				}
+			}
+		}
+		return false;
+
 	}
 }
