1533243907e100721cdf4e8cd15f6e167d507b9d
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-27 03:11:10 -0400

    Started gameLoop

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,13 +1,14 @@
 package chess;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 import board.Board;
 import board.FileRank;
-import pieces.Piece;
-import java.util.Scanner;
 public class Chess {
 	public static void main(String[] args) {
-		Board board = new Board();
-		Scanner sc = new Scanner(System.in);
+		gameLoop();
 //		board.move(new FileRank('a', 2), new FileRank('a', 4));
 //		board.printBoard();
 //		board.move(new FileRank('a', 1), new FileRank('a', 3));
@@ -21,6 +22,81 @@ public class Chess {
 //		board.move(new FileRank('a', 5), new FileRank('b', 6));
 //		board.printBoard();
 		
+
+		// should probably put game loop in this class
+	}
+	public static void gameLoop() {
+		Board board = new Board();
+		Scanner sc = new Scanner(System.in);
+		String team = "White";
+		char currentTeam = 'w';
+		boolean gameOn = true;
+		boolean illegalMove = false;
+		String startStr;
+		FileRank frStart;
+		String endStr;
+		FileRank frEnd;
+		while(gameOn) {
+			if(!illegalMove)
+				board.printBoard();
+			System.out.print(team + "'s move: ");
+			
+			String input = sc.nextLine();
+			
+			StringTokenizer st = new StringTokenizer(input, " ");
+			// TODO
+			// resign
+			// draw
+			// promotion
+			try {
+				startStr = st.nextToken();
+				frStart = getFileRank(startStr);
+				
+				endStr = st.nextToken();
+				frEnd = getFileRank(endStr);
+			} catch(NoSuchElementException e) {
+				System.out.println("Invalid input");
+				continue;
+			}
+			
+			if(board.get(frStart).getTeam() != currentTeam) {
+				System.out.println("Error: cannot control a piece on the other team");
+				continue;
+			}
+			
+			if(board.move(frStart, frEnd)) {
+				illegalMove = false;
+				if(team.equals("White")) {
+					team = "Black";
+					currentTeam = 'b';
+				} else {
+					team = "White";
+					currentTeam = 'w';
+				}
+				
+//				if(board.checkForWin()) {
+//					gameOn = false;
+//				}
+			} else {
+				System.out.println("Illegal move, try again");
+				illegalMove = true;
+			}
+		}
+	}
+	
+	private static FileRank getFileRank(String input) {
+		char file = input.charAt(0);
+		int rank = Character.getNumericValue(input.charAt(1));
+		return new FileRank(file, rank);
+	}
+	/**
+	 * This is a loop used for debugging. It allows for pieces to be moved regardless of
+	 * team or turn.
+	 */
+	private static void testLoop() {
+		Board board = new Board();
+		Scanner sc = new Scanner(System.in);
+		
 		while(true) {
 			board.printBoard();
 			System.out.print("Start: ");
@@ -32,7 +108,6 @@ public class Chess {
 			System.out.println();
 			board.move(frStart, frEnd);
 		}
-		// should probably put game loop in this class
 	}
 }
 // bR bN bB bQ bK bB bN bR  8
