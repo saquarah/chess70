4f66509c0d0cc7623ae30f25e35cb1ace155838c
NIKITA ZALA <nz132@scarletmail.rutgers.edu>
2018-10-28 16:54:02 -0400

    Castling added to king

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -22,15 +22,72 @@ public class King extends Piece{
         char e_f = end.getFile();
         int s_r = start.getRank();
         int e_r = end.getRank();
-        
-        //System.out.println(s_f);
-        //System.out.println(s_r);
-        //System.out.println(e_f);
-        //System.out.println(e_r);
-        
+
         
         if((Math.abs(s_f-e_f)<=1) && (Math.abs(s_r-e_r)<=1)) {
             return true;
+        }
+        
+        //castling
+        
+        //TODO: call check method to see if king already in check then return false
+        
+        
+        //case 1: white king on e1 and Rook on h1
+        if(s_f=='e' && e_f =='g' && s_r==1 && e_r==1 && Piece.getBoard().get(start).getTeam()=='w') {
+        	FileRank f  = new FileRank('h',1);
+        	FileRank b1  = new FileRank('f',1);
+        	FileRank b2 = new FileRank('g',1);
+        	
+        	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null) {
+        		//System.out.println("yes");
+        		if(!Piece.getBoard().get(f).hasMoved() && !this.hasMoved()) {
+        			return true;
+        		}
+        	}
+        }
+        
+        //case 2: black king on e8 and Rook on h8
+        else if(s_f=='e' && e_f =='g' && s_r==8 && e_r==8 && Piece.getBoard().get(start).getTeam()=='b') {
+        	FileRank f  = new FileRank('h',8);
+        	FileRank b1  = new FileRank('f',8);
+        	FileRank b2 = new FileRank('g',8);
+        	
+        	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null) {
+        		//System.out.println("yes");
+        		if(!Piece.getBoard().get(f).hasMoved() && !this.hasMoved()) {
+        			return true;
+        		}
+        	}
+        }
+      //Case3: white king on e1 and Rook on a1
+        else if(s_f=='e' && e_f =='c' && s_r==1 && e_r==1 && Piece.getBoard().get(start).getTeam()=='w') {
+        	FileRank f  = new FileRank('a',1);
+        	FileRank b1  = new FileRank('b',1);
+        	FileRank b2 = new FileRank('c',1);
+        	FileRank b3 = new FileRank('d',1);
+        	
+        	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null && Piece.getBoard().get(b3)==null) {
+        		//System.out.println("yes");
+        		if(!Piece.getBoard().get(f).hasMoved() && !this.hasMoved()) {
+        			return true;
+        		}
+        	}
+        }
+        
+        //case 4: black king on e8 and Rook on a8
+        else if(s_f=='e' && e_f =='c' && s_r==8 && e_r==8 && Piece.getBoard().get(start).getTeam()=='b') {
+        	FileRank f  = new FileRank('a',8);
+        	FileRank b1  = new FileRank('b',8);
+        	FileRank b2 = new FileRank('c',8);
+        	FileRank b3 = new FileRank('d',8);
+        	
+        	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null && Piece.getBoard().get(b3)==null) {
+        		//System.out.println("yes");
+        		if(!Piece.getBoard().get(f).hasMoved() && !this.hasMoved()) {
+        			return true;
+        		}
+        	}
         }
         return false;
     }
