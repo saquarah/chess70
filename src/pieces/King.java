2b711641d7fb68a65c783ae47e5e1495b4acb037
Sarah Squillace <ses333@scarletmail.rutgers.edu>
2018-10-29 15:37:20 -0400

    fixed castling

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -12,6 +12,9 @@ public class King extends Piece{
     @Override
     public boolean isValidMove(FileRank start, FileRank end) {
         // TODO Auto-generated method stub
+    	if(Piece.getBoard().underAttack(end, this.team)) { // can't move a king to a place where it'll be checkmate
+    		return false;
+    	}
         if(Piece.getBoard().get(end)!= null && Piece.getBoard().get(end).getTeam() == team) {
             //System.out.println("end is friend");
             return false;
@@ -31,12 +34,18 @@ public class King extends Piece{
         //castling
         
         //TODO: call check method to see if king already in check then return false
-        
+        if(Piece.getBoard().inCheck(this.team)) { // can't castle if the king is in check
+        	return false;
+        }
         
         //case 1: white king on e1 and Rook on h1
+        
         if(s_f=='e' && e_f =='g' && s_r==1 && e_r==1 && Piece.getBoard().get(start).getTeam()=='w') {
         	FileRank f  = new FileRank('h',1);
         	FileRank b1  = new FileRank('f',1);
+        	if(Piece.getBoard().underAttack(b1, this.team)) { // king can't pass over a space under attack
+        		return false;
+        	}
         	FileRank b2 = new FileRank('g',1);
         	
         	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null) {
@@ -51,6 +60,9 @@ public class King extends Piece{
         else if(s_f=='e' && e_f =='g' && s_r==8 && e_r==8 && Piece.getBoard().get(start).getTeam()=='b') {
         	FileRank f  = new FileRank('h',8);
         	FileRank b1  = new FileRank('f',8);
+        	if(Piece.getBoard().underAttack(b1, this.team)) {
+        		return false;
+        	}
         	FileRank b2 = new FileRank('g',8);
         	
         	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null) {
@@ -66,6 +78,9 @@ public class King extends Piece{
         	FileRank b1  = new FileRank('b',1);
         	FileRank b2 = new FileRank('c',1);
         	FileRank b3 = new FileRank('d',1);
+        	if(Piece.getBoard().underAttack(b3, this.team)) {
+        		return false;
+        	}
         	
         	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null && Piece.getBoard().get(b3)==null) {
         		//System.out.println("yes");
@@ -77,10 +92,13 @@ public class King extends Piece{
         
         //case 4: black king on e8 and Rook on a8
         else if(s_f=='e' && e_f =='c' && s_r==8 && e_r==8 && Piece.getBoard().get(start).getTeam()=='b') {
-        	FileRank f  = new FileRank('a',8);
+        	FileRank f  = new FileRank('a',8); // these are not meaningful variable names
         	FileRank b1  = new FileRank('b',8);
         	FileRank b2 = new FileRank('c',8);
         	FileRank b3 = new FileRank('d',8);
+        	if(Piece.getBoard().underAttack(b3, this.team)) {
+        		return false;
+        	}
         	
         	if(Piece.getBoard().get(f) instanceof Rook && Piece.getBoard().get(b1)==null && Piece.getBoard().get(b2)==null && Piece.getBoard().get(b3)==null) {
         		//System.out.println("yes");
